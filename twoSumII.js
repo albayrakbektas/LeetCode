ecaf53202a64f794d1f8f3c33b103ec0a42e507c
Bektas Albayrak <albayrakbektas44@gmail.com>
2022-01-04 15:42:42 +0300

    second Solution
    binary search

--- twoSumII.js
+++ twoSumII.js
@@ -16,3 +16,23 @@ const twoSum = function(numbers, target) {
     }
     return res
 };
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSumExpected = function (numbers, target) {
+    let left = 0
+    let right = numbers.length - 1
+    while (left <= right) {
+        let sum = numbers[left] + numbers[right]
+        if (target === sum) {
+            return [left+1, right+1]
+        } else if (target > sum) {
+            left++
+        } else {
+            right--
+        }
+    }
+}
